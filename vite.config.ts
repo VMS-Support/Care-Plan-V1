f8223a2f059fbaff427f5c648bd3c38b59138d04
Abdul Rahman Al-Adhubi <abdulesalne@gmail.com>
2026-07-31 10:50:29 +0100

    removed index.html from main root

--- vite.config.ts
+++ vite.config.ts
@@ -20,14 +20,23 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 //   },
 // });
 
+// export default defineConfig({
+//   tanstackStart: {
+//     server: { entry: "server" },
+//   },
+//   vite: {
+//     base: "",
+//     build: {
+//       assetsDir: "assets",
+//     },
+//   },
+// });
+
 export default defineConfig({
   tanstackStart: {
     server: { entry: "server" },
   },
-  vite: {
-    base: "",
-    build: {
-      assetsDir: "assets",
-    },
+  nitro: {
+    preset: "vercel",
   },
 });
